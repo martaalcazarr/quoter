9531416908bf93c284d33caf8639b4b1dc960daa
martaalcazarr <martaalcazarredondo@gmail.com>
2023-08-11 21:39:53 -0400

    add form

--- src/constants/index.js
+++ src/constants/index.js
@@ -9,3 +9,8 @@ export const YEARS = Array.from(
     new Array(65),
     (value, index) => YEARMAX -index
 );
+
+export const PLANS = [
+    {id:1, name: 'Basic' },
+    {id:2, name: 'Complete'},
+];
