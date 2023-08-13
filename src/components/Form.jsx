323707ef605ce06dfadbbce56c94e82f491cd34f
martaalcazarr <martaalcazarredondo@gmail.com>
2023-08-13 00:22:55 -0400

    add state to form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,14 +1,23 @@
 import { Fragment } from "react"
 import { COMPANIES, YEARS, PLANS } from "../constants"
+import useQuote from "../hooks/useQuoter"
 
 const Form = () => {
+
+    const {data, handleChangeData} = useQuote()
   return (
     <>
 
     <form>
         <div className="my-5">
             <label className="block mb-3 font-bold text-gray-400">Company</label>
-            <select name="company" id="company" className="w-full p-3 bg-white border-gray-200">
+            <select 
+                name="company" 
+                id="company" 
+                className="w-full p-3 bg-white border-gray-200"
+                onChange={e => handleChangeData(e)}
+                value={data.company}
+            >
             <option value="0">--Select your company</option>
             {COMPANIES.map(company => (
                 <option
@@ -22,7 +31,13 @@ const Form = () => {
 
         <div className="my-5">
             <label className="block mb-3 font-bold text-gray-400">Year of birth</label>
-            <select name="company" id="company" className="w-full p-3 bg-white border-gray-200">
+            <select 
+                name="year" 
+                id="year" 
+                className="w-full p-3 bg-white border-gray-200"
+                onChange={e => handleChangeData(e)}
+                value={data.year}
+            >
             <option value="0">--Select your year of birth</option>
             {YEARS.map(year => (
                 <option
@@ -46,6 +61,7 @@ const Form = () => {
                             type="radio"
                             name="plan"
                             value={plan.id}
+                            onChange={e => handleChangeData(e)}
                         />
                     </Fragment>
                 ))}
