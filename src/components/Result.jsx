b88471a0301f0582ca153de21bd0323334ea94bf
martaalcazarr <martaalcazarredondo@gmail.com>
2023-08-17 17:53:56 -0400

    add companies to result

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,11 +1,21 @@
 import useQuote from "../hooks/useQuoter"
+import { COMPANIES, PLANS } from "../constants"
 
 const Result = () => {
-    const {result} = useQuote()
+    const {result, data} = useQuote()
+    const {company, year, plan} = data
+
+    const [companyName] = COMPANIES.filter(c => c.id === Number(company))
 
     if(result === 0) return null
   return (
-    <div>{result}</div>
+    <div className="bg-gray-100 text-center mt-5 p-5 shadow">
+        <h2 className="text-gray-600 font-black text-3xl">Your choices</h2>
+        <p className="my-2">
+            <span className="font-bold">Company: </span>
+            {companyName.name}
+        </p>
+    </div>
   )
 }
 
